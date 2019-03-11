9497e1bf86378fd8825d72ec1bbb9393ed39946a
Natasha Chernis <natashachernis@gmail.com>
2019-03-11 06:02:49 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,6 +5,8 @@ def nyc_pigeon_organizer(data)
   pigeon_names = data.map {|category, attributes|
     attributes.map {|attribute, pigeon_names| pigeon_names}
   }.flatten.uniq!
-  binding.pry
-  pigeon_names
+
+  pigeon_list = {}
+  pigeon_names.each {|pigeon| pigeon_list[pigeon] = {}
+  }
 end
