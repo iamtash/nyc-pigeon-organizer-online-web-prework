7846007a9ddfc8d0e2dbd6ca9ff0c7ae8757b235
Natasha Chernis <natashachernis@gmail.com>
2019-03-11 05:56:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,10 @@
 require "pry"
 
 def nyc_pigeon_organizer(data)
-  binding.pry
+  
   pigeon_names = data.map {|category, attributes|
     attributes.map {|attribute, pigeon_names| pigeon_names}
   }
+  binding.pry
+  pigeon_names
 end
